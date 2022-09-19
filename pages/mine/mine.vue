be1f607d5c7600ac940281c618ed78a2b2f4904f
hwzhu <howardzz@foxmal.com>
2022-09-19 22:19:29 +0800

    tabbar ok

---
+++ pages/mine/mine.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view>mine</view>	
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
