b38ee7f255af3a7fbc6d0f3feba96e3df2ff432b
hwzhu <howardzz@foxmal.com>
2022-09-21 23:13:22 +0800

    [FEAT] show userinfo in mine-page ok

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,18 +1,35 @@
 <template>
 	<view>
-		<uni-card title="基础卡片" sub-title="副标题" extra="额外信息" :thumbnail="vuex_user.avatar_url"></uni-card>
+		<uni-card :title="vuex_user.nickname" :sub-title="vuex_user.campus" :extra="authTag"
+			:thumbnail="vuex_user.avatar_url"></uni-card>
 	</view>
 </template>
 
 <script>
+import store from '@/store/index.js';
 export default {
 	data() {
 		return {
-
+			authTag: '未认证'
 		}
 	},
+	onLoad() {
+		this.refreshAuthTag()
+	},
 	methods: {
-
+		refreshAuthTag() {
+			switch (store.state.vuex_user.role) {
+				case "STUDENT":
+					this.authTag = "已认证"
+					break
+				case "VISITOR":
+					this.authTag = "未认证"
+					break
+				case "PENDING":
+					this.authTag = "审核中"
+					break
+			}
+		}
 	}
 }
 </script>
