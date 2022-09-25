e7fa4542ec4819b0b77fdece3605ff2a46e00796
hwzhu <howardzz@foxmal.com>
2022-09-25 23:28:38 +0800

    [FEAT] adjusting input realname

--- page_subject/pages/auth.vue
+++ page_subject/pages/auth.vue
@@ -1,23 +1,28 @@
 <template>
-	<view>
-        <view>auth</view>
-	</view>
+    <view>
+        <uni-forms :modelValue="formData" label-position="left" label-width="100" label-align="center">
+            <uni-forms-item label="真实姓名" name="name">
+                <u-input type="text" v-model="realname" placeholder="仅用于身份审核" inputAlign="center" border="none" />
+            </uni-forms-item>
+        </uni-forms>
+    </view>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			title: 'Hello',
-		}
-	},
-	onLoad() {
-        
-	},
-	methods: {
-	}
+    data() {
+        return {
+            realname: '',
+        }
+    },
+    onLoad() {
+
+    },
+    methods: {
+    }
 }
 </script>
 
 <style>
+
 </style>
