8fd73cdcf81d7d54abb676b1db66726ec48130ed
hwzhu <howardzz@foxmal.com>
2022-09-25 22:39:33 +0800

    [FEAT] add auth entry button

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -2,6 +2,14 @@
 	<view>
 		<uni-card :title="vuex_user.nickname" :sub-title="vuex_user.campus" :extra="authTag"
 			:thumbnail="vuex_user.avatar_url"></uni-card>
+		<uni-grid :column="4" :highlight="true" :showBorder="false" :square="false" @change="gridItemOnClick">
+			<uni-grid-item v-for="(item, index) in grids" :index="index" :key="index">
+				<view class="grid-item-box" style="background-color: #fff;">
+					<u-icon size="40" name="/static/mine/grid-auth.png"></u-icon>
+					<view>{{item.text}}</view>
+				</view>
+			</uni-grid-item>
+		</uni-grid>
 	</view>
 </template>
 
@@ -10,7 +18,11 @@ import store from '@/store/index.js';
 export default {
 	data() {
 		return {
-			authTag: '未认证'
+			authTag: '未认证',
+			grids: [{
+				icon: "/static/mine/grid-auth.png",
+				text: "身份认证",
+			}]
 		}
 	},
 	onLoad() {
@@ -29,6 +41,14 @@ export default {
 					this.authTag = "审核中"
 					break
 			}
+		},
+
+		gridItemOnClick(e) {
+			console.log("grid item clicked: ", e)
+			switch (e.detail.index) {
+				case 1:
+					uni.navigateTo({url: 'HM-hand/HM-hand'})
+			}
 		}
 	}
 }
@@ -46,5 +66,17 @@ export default {
 
 .bg-blue {
 	background: #2979ff;
+}
+
+.grid-item-box {
+	flex: 1;
+	// position: relative;
+	/* #ifndef APP-NVUE */
+	display: flex;
+	/* #endif */
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 15px 0;
 }
 </style>
