1153d07b0227832f04ccf0ae88fef593230e51b8
hwzhu <howardzz@foxmal.com>
2022-09-25 14:24:29 +0800

    [FEAT] haha, seems importing TIM ok, just compile pass

--- App.vue
+++ App.vue
@@ -2,6 +2,20 @@
 import common from './common/common.js';
 import { getTokenFromServer, getUserInfoFromServer } from './config/api.js';
 export default {
+	mounted() {
+		/**官网有很多关于关于sdk 其他的监听方法（比如：有新的消息，用户资料更新等等）
+		 * 详情可对照： https://imsdk-1252463788.file.myqcloud.com/IM_DOC/Web/SDK.html
+		 * 监听的含义：服务端发生了数据变更---前端全局可以接收到变更通知--前端就可以自动触发某个事件来更新相应数据
+		 * */
+		// 登录成功后会触发 SDK_READY 事件，该事件触发后，可正常使用 SDK 接口
+		this.tim.on(this.$TIM.EVENT.SDK_READY, this.onReadyStateUpdate, this);
+		// 收到新消息
+		this.tim.on(this.$TIM.EVENT.MESSAGE_RECEIVED, this.onReceiveMessage);
+		// 会话列表更新
+		this.tim.on(this.$TIM.EVENT.CONVERSATION_LIST_UPDATED, event => {
+			this.$store.commit("updateConversationList", event.data);
+		});
+	},
 
 	onLaunch: async function () {
 		await this.getToken()
@@ -16,19 +30,32 @@ export default {
 	},
 	methods: {
 		async getToken() {
-			var response = await uni.login({provider: 'weixin'})
+			var response = await uni.login({ provider: 'weixin' })
 			var wx_login_code = response[1].code
 			console.log("[DEBUG] wx_login_code: ", wx_login_code)
-			var resp = await getTokenFromServer({wx_login_code: wx_login_code})	
+			var resp = await getTokenFromServer({ wx_login_code: wx_login_code })
 			this.$u.vuex('vuex_token', resp.token);
 			console.log("[DEBUG] token: ", this.$store.state.vuex_token)
 		},
 
 		async getUserInfo() {
-			var resp = await getUserInfoFromServer()	
+			var resp = await getUserInfoFromServer()
 			this.$u.vuex('vuex_user', resp);
 			console.log("[DEBUG] user info: ", this.$store.state.vuex_user)
-		}
+		},
+
+		onReadyStateUpdate({ name }) {
+			const isSDKReady = name === this.$TIM.EVENT.SDK_READY ? true : false;
+			//自动监听并更新 sdk 的ready 状态 （未登录是 notReady  登录后是ready）
+			this.$store.commit("toggleIsSDKReady", isSDKReady);
+			//sdk ready 后  肯定完成了登录操作    这里可以获取用户存储在im的基础信息/离线消息/黑名单列表
+		},
+
+		onReceiveMessage({ data: messageList }) {
+			// this.handleAt(messageList);
+			this.$store.commit("pushCurrentMessageList", messageList);
+		},
+		//根据消息列表请求聊天对象的用户信息 并完成数据拼接
 	}
 
 }
--- main.js
+++ main.js
@@ -5,6 +5,10 @@ import Vue from 'vue'
 import uView from '@/uni_modules/uview-ui'
 import store from '@/store';
 
+import tim from './commen/tim/tim.js'
+import commen from './commen/commen.js'
+import TIM from 'tim-js-sdk'
+
 Vue.prototype.$store = store
 Vue.config.productionTip = false
 
@@ -12,6 +16,10 @@ Vue.config.productionTip = false
 let vuexStore = require("@/store/$u.mixin.js");
 Vue.mixin(vuexStore);
 
+Vue.config.productionTip = false
+Vue.prototype.tim = tim.tim  			//tim sdk 引入后生成的tim服务
+Vue.prototype.$TIM = TIM				//tim 的状态/事件 常量
+Vue.prototype.$commen = commen
 
 App.mpType = 'app'
 
