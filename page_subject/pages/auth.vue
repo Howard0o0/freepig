8c8c641a31acdaecdd9d30873d0fd4ed97548959
hwzhu <howardzz@foxmal.com>
2022-09-27 22:17:42 +0800

    [FEAT] add dual auto button

--- page_subject/pages/auth.vue
+++ page_subject/pages/auth.vue
@@ -18,6 +18,24 @@
                 <uni-data-select v-model="formData.selectedKickoffYear" :localdata="kickoffYearList"></uni-data-select>
             </u-form-item>
         </u--form>
+
+        <uni-row class="demo-uni-row " :width=730>
+            <uni-col :span="2">
+                <view class="demo-uni-col"></view>
+            </uni-col>
+            <uni-col :span="9">
+                <u-button type="primary" size="normal" text="学校邮箱认证"></u-button>
+            </uni-col>
+            <uni-col :span="2">
+                <view class="demo-uni-col"></view>
+            </uni-col>
+            <uni-col :span="9">
+                <u-button type="primary" size="normal" text="学生证认证"></u-button>
+            </uni-col>
+            <uni-col :span="2">
+                <view class="demo-uni-col"></view>
+            </uni-col>
+        </uni-row>
     </view>
 </template>
 
@@ -61,5 +79,17 @@ export default {
 </script>
 
 <style>
+.demo-uni-row {
+    margin-bottom: 10px;
+    /* QQ、字节小程序文档写有 :host，但实测不生效 */
+    /* 百度小程序没有 :host，需要设置block */
+    /* #ifdef MP-TOUTIAO || MP-QQ || MP-BAIDU */
+    display: block;
+    /* #endif */
+}
 
+.demo-uni-col {
+    height: 36px;
+    border-radius: 4px;
+}
 </style>
