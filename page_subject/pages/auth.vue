cf015e0f95a6467b9052533c2721c96a2954348b
hwzhu <howardzz@foxmal.com>
2022-09-27 21:47:58 +0800

    [FEAT] init form input framework of auth page

--- page_subject/pages/auth.vue
+++ page_subject/pages/auth.vue
@@ -1,8 +1,21 @@
 <template>
     <view>
-        <u--form labelPosition="left" :model="formData" :rules="rules">
-            <u-form-item label="真实姓名" labelWidth="80" prop="realname" borderBottom>
-                <u--input v-model="formData.realname" inputAlign="center" border="none"></u--input>
+        <u--form labelPosition="left" :model="formData">
+            <u-form-item label="真实姓名" labelWidth="80" borderBottom>
+                <u--input v-model="formData.realname" placeholder="仅用于身份认证" inputAlign="center" border="none">
+                </u--input>
+            </u-form-item>
+            <u-form-item label="学校" labelWidth="80" borderBottom>
+                <u--text :text="formData.selectedCampus.name" align="center"></u--text>
+            </u-form-item>
+            <u-form-item label="专业" labelWidth="80" borderBottom>
+                <uni-data-select v-model="formData.selectedMajor.id" :localdata="majorList"></uni-data-select>
+            </u-form-item>
+            <u-form-item label="学历" labelWidth="80" borderBottom>
+                <uni-data-select v-model="formData.selectedDegreeid" :localdata="degreeList"></uni-data-select>
+            </u-form-item>
+            <u-form-item label="入学年份" labelWidth="80" borderBottom>
+                <uni-data-select v-model="formData.selectedKickoffYear" :localdata="kickoffYearList"></uni-data-select>
             </u-form-item>
         </u--form>
     </view>
@@ -12,7 +25,31 @@
 export default {
     data() {
         return {
-            realname: '',
+            formData: {
+                realname: "",
+                selectedCampus: {
+                    id: 0,
+                    name: "请选择学校"
+                },
+                selectedMajor: {
+                    id: 0,
+                    name: ""
+                },
+                selectedDegree: "本科",
+                selectedKickoffYear: 2015,
+            },
+            majorList: [
+                { value: 1, text: "计算机" },
+                { value: 2, text: "数学" },
+            ],
+            degreeList: [
+                { value: 1, text: "专科" },
+                { value: 2, text: "本科" },
+            ],
+            kickoffYearList: [
+                { value: 1, text: "2015" },
+                { value: 2, text: "2016" },
+            ],
         }
     },
     onLoad() {
