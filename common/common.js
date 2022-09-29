fedcbd7d6d2c969f584641ad13b4c6b80ba2af85
hwzhu <howardzz@foxmal.com>
2022-09-29 22:26:19 +0800

    [FEAT] search bar of pickCampus page ok

--- common/common.js
+++ common/common.js
@@ -1,8 +1,32 @@
 var token = ""
 const baseURL = "https://182.92.109.123:8080/api/v1"
 
+//防抖  
+function debounce(fn, wait = 500, isImmediate = false) {
+    let timerId = null;
+    let flag = true;
+    if (isImmediate) {
+        return function () {
+            clearTimeout(timerId);
+            if (flag) {
+                fn.apply(this, arguments);
+                flag = false
+            }
+            timerId = setTimeout(() => {
+                flag = true
+            }, wait)
+        }
+    }
+    return function () {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            fn.apply(this, arguments)
+        }, wait)
+    }
+}
 
-export default {  
+export default {
     token,
-    baseURL
+    baseURL,
+    debounce
 }
--- page_subject/pages/pick_campus.vue
+++ page_subject/pages/pick_campus.vue
@@ -1,39 +1,77 @@
 <template>
     <view>
+        <u-search placeholder="可输入学校关键字" @change="onSearchContentChange" :showAction="false"></u-search>
         <uni-list>
-            <uni-list-item v-for="(item,index) in campusList" :key="index" :title="item.name" clickable
+            <uni-list-item v-for="(item,index) in campusListToShow" :key="index" :title="item.name" clickable
                 @click="campusItemOnClick(item.id)"></uni-list-item>
         </uni-list>
     </view>
 </template>
 
 <script>
+
 export default {
     data() {
         return {
-            campusList:
-                [
-                    {
-                        id: 1,
-                        name: "清华大学"
-                    },
-                    {
-                        id: 2,
-                        name: "武汉大学"
-                    },
-                    {
-                        id: 3,
-                        name: "武汉理工大学"
-                    }
-                ]
+            campusListToShow: [
+                {
+                    id: 1,
+                    name: "清华大学"
+                },
+                {
+                    id: 2,
+                    name: "武汉大学"
+                },
+                {
+                    id: 3,
+                    name: "武汉理工大学"
+                }
+            ],
+            campusFullListkey: [
+                {
+                    id: 1,
+                    name: "清华大学"
+                },
+                {
+                    id: 2,
+                    name: "武汉大学"
+                },
+                {
+                    id: 3,
+                    name: "武汉理工大学"
+                }
+            ],
         }
     },
     onLoad() {
-
+        // fill the campusFullListkey if empty
+        // then fill the campusListToShow using ycampusFullListkey 
     },
     methods: {
         campusItemOnClick(campusID) {
-            console.log('clicked campus item: ', campusID)
+            console.log('choosed campusID: ', campusID)
+        },
+
+        onSearchContentChangeImpl(content) {
+            console.log('search content: ', content)
+            this.refreshCampusListToShow(content)
+        },
+
+        onSearchContentChange(content) {
+            this.$u.debounce(() => this.onSearchContentChangeImpl(content))
+        },
+
+        refreshCampusListToShow(keyword) {
+            var campusListToShow = []
+            var i
+            for (i in this.campusFullListkey) {
+                var campus = this.campusFullListkey[i]
+                if (campus.name.indexOf(keyword) == -1) {
+                    continue
+                }
+                campusListToShow.push(campus)
+            }
+            this.campusListToShow = campusListToShow
         }
     }
 }
