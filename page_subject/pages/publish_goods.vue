452ecd598f0ec7e4e14373e62e4a312daef2e436
hwzhu <howardzz@foxmal.com>
2022-10-01 23:23:37 +0800

    [FEAT] pick images in page publish_goods ok

--- page_subject/pages/publish_goods.vue
+++ page_subject/pages/publish_goods.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-
+	<view class="centerAlign">
+		<uni-easyinput type="textarea" autoHeight="true" v-model="goodsDesc" :placeholder="goodsDescPlaceHolder"
+			inputBorder="false" />
+		<u-album :urls="choosedImageURLs"></u-album>
+		<u--image showLoading="false" src="/page_subject/static/add_pic_btn.png" width="200rpx" height="200rpx"
+			@click="addPictureBtnOnClick"></u--image>
 	</view>
 </template>
 
@@ -12,15 +16,38 @@ import { utils } from '../../common/common.js';
 export default {
 	data() {
 		return {
-
+			goodsDesc: "",
+			goodsDescPlaceHolder: "在这里描述下宝贝的转手原因、入手渠道、规格以及新旧程度和使用感受吧, 会有助于更快的转手喔~ ^o^",
+			choosedImageURLs: ["", "", "", "", "", ""]
 		}
 	},
 	methods: {
+		confirmBtnOnClick() {
 
+		},
+
+		async addPictureBtnOnClick() {
+			var res = await uni.chooseImage({
+				count: 6,
+				sizeType: ['compressed'],
+			});
+			var selectedImageFiles = res[1].tempFiles
+			console.log('[DEBUG] selected images: ', selectedImageFiles)
+			var choosedImageURLs = []
+			for (let i = 0; i < selectedImageFiles.length; i++) {
+				choosedImageURLs.push(selectedImageFiles[i].path)
+			}
+			this.choosedImageURLs = choosedImageURLs
+		}
 	}
 }
 </script>
 
 <style>
-
+.centerAlign {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
 </style>
