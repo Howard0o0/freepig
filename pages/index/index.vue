50b5c98edc178d2f0a4803a5175df940e372ca17
hwzhu <howardzz@foxmal.com>
2022-10-04 09:29:08 +0800

    [FEAT] init goodsList waterfall with fake data

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,24 @@
 <template>
 	<view>
-		<v-tabs pills="true" activeColor="#fff" fontSize="40rpx" v-model="currTabIndex" :tabs="tabNameList" @change="tabOnChange"></v-tabs>
-		<u-divider text="为你推荐"></u-divider>
+		<!-- 商品标签 -->
+		<v-tabs pills="true" activeColor="#fff" fontSize="40rpx" v-model="currTabIndex" :tabs="tabNameList"
+			@change="tabOnChange"></v-tabs>
+
+		<!-- 商品列表 -->
+		<view class="goods-list">
+			<view class="product-list">
+				<view class="product" v-for="(goods) in goodsList" :key="goods.goods_id" @tap="toGoods(goods)">
+					<image mode="widthFix" :src="goods.img"></image>
+					<view class="name">{{goods.name}}</view>
+					<view class="info">
+						<view class="price">{{goods.price}}</view>
+						<view class="slogan">{{goods.slogan}}</view>
+					</view>
+				</view>
+			</view>
+			<view class="loading-text">{{loadingText}}</view>
+		</view>
+
 
 		<uni-fab ref="fab" horizontal="right" vertical="bottom" @fabClick="createGoodsBtnOnClick" />
 	</view>
@@ -18,6 +35,20 @@ export default {
 			currTabIndex: 0,
 			tabNameList: ['军事', '国内', '新闻', '军事', '国内', '新闻', '军事', '国内', '新闻'],
 			tagList: [],
+
+			goodsList: [
+				{ goods_id: 0, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 1, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 2, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 3, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 4, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 5, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 6, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 7, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 8, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' },
+				{ goods_id: 9, img: '/static/choose_image.png', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' }
+			],
+			loadingText: "正在加载...",
 		}
 	},
 
@@ -25,7 +56,42 @@ export default {
 		this.renderTabNameList()
 	},
 
+	//下拉刷新，需要自己在page.json文件中配置开启页面下拉刷新 "enablePullDownRefresh": true
+	onPullDownRefresh() {
+		setTimeout(() => {
+			this.reload();
+			uni.stopPullDownRefresh();
+		}, 1000);
+	},
+	//上拉加载，需要自己在page.json文件中配置"onReachBottomDistance"
+	onReachBottom() {
+		uni.showToast({ title: '触发上拉加载' });
+		let len = this.goodsList.length;
+		if (len >= 40) {
+			this.loadingText = "到底了";
+			return false;
+		} else {
+			this.loadingText = "正在加载...";
+		}
+		// TODO: append some goods into goodsList
+		// let end_goods_id = this.goodsList[len - 1].goods_id;
+		// for (let i = 1; i <= 10; i++) {
+		// 	let goods_id = end_goods_id + i;
+		// 	let p = { goods_id: goods_id, img: '/static/img/goods/p' + (goods_id % 10 == 0 ? 10 : goods_id % 10) + '.jpg', name: '商品名称商品名称商品名称商品名称商品名称', price: '￥168', slogan: '1235人付款' };
+		// 	this.goodsList.push(p);
+		// }
+	},
+
 	methods: {
+
+		//商品跳转
+		toGoods(e) {
+			uni.showToast({ title: '商品' + e.goods_id, icon: "none" });
+			// uni.navigateTo({
+			// 	url: '../goods'
+			// });
+		},
+
 		async renderTabNameList() {
 			var resp = await api.getTagList()
 			if (resp.code != api.SUCCESS_CODE) { return false }
@@ -63,7 +129,8 @@ export default {
 }
 </script>
 
-<style>
+
+<style lang="scss">
 .content {
 	display: flex;
 	flex-direction: column;
@@ -88,5 +155,114 @@ export default {
 .title {
 	font-size: 36rpx;
 	color: #8f8f94;
+}
+
+.icon {
+	font-size: 26upx;
+}
+
+.header {
+	width: 92%;
+	padding: 0 4%;
+	height: 79upx;
+	display: flex;
+	justify-content: space-around;
+	align-items: flex-end;
+	position: fixed;
+	top: 0;
+	z-index: 10;
+	background-color: #fff;
+	border-bottom: solid 1upx #eee;
+
+	.target {
+		width: 20%;
+		height: 60upx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 28upx;
+		margin-bottom: -2upx;
+		color: #aaa;
+
+		&.on {
+			color: #555;
+			border-bottom: 4upx solid #f06c7a;
+			font-weight: 600;
+			font-size: 30upx;
+		}
+
+
+	}
+}
+
+.place {
+
+	background-color: #ffffff;
+	height: 100upx;
+
+}
+
+.goods-list {
+	.loading-text {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 60upx;
+		color: #979797;
+		font-size: 24upx;
+	}
+
+	.product-list {
+		width: 92%;
+		padding: 0 4% 3vw 4%;
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+
+		.product {
+			width: 48%;
+			border-radius: 20upx;
+			background-color: #fff;
+			margin: 0 0 15upx 0;
+			box-shadow: 0upx 5upx 25upx rgba(0, 0, 0, 0.1);
+
+			image {
+				width: 100%;
+				border-radius: 20upx 20upx 0 0;
+			}
+
+			.name {
+				width: 92%;
+				padding: 10upx 4%;
+				display: -webkit-box;
+				-webkit-box-orient: vertical;
+				-webkit-line-clamp: 2;
+				text-align: justify;
+				overflow: hidden;
+				font-size: 30upx;
+			}
+
+			.info {
+				display: flex;
+				justify-content: space-between;
+				align-items: flex-end;
+				width: 92%;
+				padding: 10upx 4% 10upx 4%;
+
+				.price {
+					color: #e65339;
+					font-size: 30upx;
+					font-weight: 600;
+				}
+
+				.slogan {
+					color: #807c87;
+					font-size: 24upx;
+				}
+			}
+		}
+
+	}
 }
 </style>
