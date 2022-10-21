f3d5047ad0e118961cf5084d32d922ec905adae9
hwzhu <howardzz@foxmal.com>
2022-10-21 23:02:21 +0800

    [REFACTOR] adjust contact_us page, center align

--- page_subject/pages/contact_us.vue
+++ page_subject/pages/contact_us.vue
@@ -1,7 +1,9 @@
 <template>
-	<view class="left-rigth-margin">
-		<image src="/page_subject/static/about_us.jpeg" mode="aspectFit"></image>
-		<view>{{aboutFreePig}}</view>
+	<view class="left-rigth-margin centerAlign">
+		<image class="logo" src="/page_subject/static/logo.png" mode="aspectFit"></image>
+		<view>v0.1.0 beta</view>
+		<view>{{aboutFreePigParagraph0}}</view>
+		<view>{{aboutFreePigParagraph1}}</view>
 		<u-form-item leftIcon="email" >
 			<view>howardzz@foxmail.com</view>
 		</u-form-item>
@@ -17,7 +19,8 @@ import { utils } from '../../common/common.js';
 export default {
 	data() {
 		return {
-			aboutFreePig: "作为一名毕业已久的体验者，在试用这款产品的一周里，我始终没有产生交流欲望和依赖感。Summer对我没有足够的吸引力，而我实际上也并不在它的核心目标用户群之中。可能是我早已脱离了对校园的依赖，也可能移动互联网时代，我越来越习惯于虚拟人设间的自嗨，对现实反而失去了感觉。",
+			aboutFreePigParagraph0: "多啦猪是由清华-武大的学生团队联合开发的便捷校园宝贝发布平台, 致力于打造一个可靠且方便的校园宝贝信息发布平台, 让大家在这里可以方便的发布手头上的闲置宝贝, 寻找心仪的优质宝贝",
+			aboutFreePigParagraph1: "如果你对产品的使用有疑惑或不满, 以及相关的改进意见都欢迎骚扰呀, 嘿嘿",
 		}
 	},
 
@@ -31,5 +34,10 @@ export default {
 </script>
 
 <style>
+
+.logo {
+	width: 200rpx;
+	height: 200rpx;
+}
 
 </style>
