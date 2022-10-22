97d987eb2e8ec68729256650b834086d2748ad55
hwzhu <howardzz@foxmal.com>
2022-10-22 17:28:26 +0800

    [FEAT] show the recommend user list by mock data ok

--- page_subject/pages/recommend.vue
+++ page_subject/pages/recommend.vue
@@ -1,6 +1,12 @@
 <template>
 	<view class="left-rigth-margin centerAlign">
-		<button open-type="share">生成内推链接</button>
+		<scroll-view scroll-y="true" class="scroll-Y">
+			<view class="row" v-for="(user,index) in authUserList" :key="index">
+				<uni-list-chat :avatar-circle="false" :title="user.wx_nickname" :avatar="user.wx_avatar_url"
+					:note="joinCampusAndMajorInfo(user.campus_name, user.major_name)" :time="parseUserRole(user.role)" ></uni-list-chat>
+			</view>
+		</scroll-view>
+		<button open-type="share" type="primary">生成内推链接</button>
 	</view>
 </template>
 
@@ -9,10 +15,19 @@
 import { api } from '../../config/api.js';
 import { utils } from '../../common/common.js';
 
-
 export default {
 	data() {
 		return {
+			authUserList: [
+				{
+					wx_nickname: "wx_nickname",
+					wx_avatar_url: "https://ddp-freepig.oss-cn-beijing.aliyuncs.com/images/default-avatar.png",
+					gender: "MALE",
+					campus_name: "武汉大学",
+					major_name: "计算机",
+					role: "PENDING"
+				}
+			],
 		}
 	},
 
@@ -30,14 +45,29 @@ export default {
 	},
 
 	methods: {
+		joinCampusAndMajorInfo(campusName, majorName) {
+			return campusName + " | " + majorName
+		},
 
+		parseUserRole(role) {
+			switch (role) {
+				case "STUDENT":
+					return "已认证"
+				case "VISITOR":
+					return "未认证"
+				case "PENDING":
+					return "审核中"
+				case "VERIFY_BROKEN":
+					return "审核未通过"
+			}
+			return ""
+		},
 	}
 }
 </script>
 
 <style>
-.logo {
-	width: 200rpx;
-	height: 200rpx;
+.scroll-Y {
+	height: 300rpx;
 }
 </style>
