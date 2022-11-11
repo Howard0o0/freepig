840d53125318edce7152a3974f0b25fcf36437c8
hwzhu <howardzz@foxmal.com>
2022-11-11 13:55:56 +0800

    chore: use prd server

--- config/request.js
+++ config/request.js
@@ -2,8 +2,8 @@ module.exports = (vm) => {
     // 初始化请求配置
     uni.$u.http.setConfig((config) => {
         /* config 为默认全局配置*/
-        config.baseURL = 'http://localhost:8080/api/v1'; /* 根域名 */
-        // config.baseURL = 'https://secondmarket.top/api/v1'; /* 根域名 */
+        // config.baseURL = 'http://localhost:8080/api/v1'; /* 根域名 */
+        config.baseURL = 'https://secondmarket.top/api/v1'; /* 根域名 */
         return config
     })
 
