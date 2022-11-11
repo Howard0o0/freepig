a2ab86327b956be4ce3802cb8138bc65510a30dc
hwzhu <howardzz@foxmal.com>
2022-11-11 14:11:25 +0800

    feat: add page chat room into whitelist

--- config/page_interceptor.js
+++ config/page_interceptor.js
@@ -12,6 +12,7 @@ const whiteList = [
     "/page_subject/pages/set_userinfo",
     "/page_subject/pages/contact_us",
     "/page_subject/pages/admin_login",
+    "/pages/tim/room",
 ]
 
 function isPageInWhiteList(url) {
