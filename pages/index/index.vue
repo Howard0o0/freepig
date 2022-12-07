721de27fef7f49c8179c7dd4f826b6fea5977a1c
hwzhu <howardzz@foxmal.com>
2022-12-07 22:06:43 +0800

    chore: change log

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -110,7 +110,7 @@ export default {
 			console.log('share from button', res.target)
 		}
 		return {
-			title: '欢迎加入蹲蹲蹲小集市',
+			title: '蹲蹲蹲小集市',
 			path: '/pages/index/index',
 			imageUrl: '/page_subject/static/logo.png',
 		}
